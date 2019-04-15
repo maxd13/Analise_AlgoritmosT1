aa414f8538edb4704d034c3a636959bfb6d8f808
David Beyda <dbeyda@poli.ufrj.br>
2019-04-15 11:58:46 -0300

    Realizando experimentos

---
+++ experiments.py
@@ -0,0 +1,63 @@
+from linear_select import median_of_medians as linear_selection
+from random import uniform
+
+import time
+
+def avg(ls):
+    return sum(ls)/len(ls)
+
+
+def sort_selection(ls, k):
+    assert type(k) is int
+    assert type(ls) is list
+    
+    n = len(ls)
+    if k <= 0 or k > n:
+         raise Exception(f"""parameter k should be greater than 0 and less then the size of the input list.
+                        Was = {k}.
+                        Size of list was = {n}.
+                        """
+                        )
+    return sorted(ls)[k-1]
+
+
+def main():
+    n_instances = 10
+    sizes = list(range(1000, 10001, 1000))
+    data = dict()
+    average_times = dict()
+
+    for size in sizes:
+        data[size] = []
+        for k in range(n_instances):
+            data[size].append( [uniform(0, 100000) for i in range(size)] )
+    
+    for size in sizes:
+        sort_times = []
+        linear_times = []
+        for i in range(n_instances):
+            k = size // 2
+            # start time taking
+            start = time.time()
+            sort_selection(data[size][i], k)
+            # stop counting time
+            end = time.time()
+            sort_times.append(end-start)
+
+            # start taking time
+            start = time.time()
+            linear_selection(data[size][i], k)
+            # stop taking time
+            end = time.time()
+            linear_times.append(end-start)
+
+        average_times[size] = {
+            'linear': avg(linear_times),
+            'sort': avg(sort_times)
+        }
+
+    print(average_times)
+
+
+if __name__ == '__main__':
+    main()
